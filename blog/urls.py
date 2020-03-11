89318f1d0abaa42e035628b52b27fa41d75426b8
Junfeng ou <junfengou@gmail.com>
2020-03-11 08:24:58 -0500

    third commit of my website prototype

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.login, name='blog-login'),
+    path('', views.user_login, name='blog-login'),
+    #path('', auth_views.LoginView.as_view, name='login'),
+    #path('logout', auth_views.LogoutView.as_view, name='logout'),
+
+
     path('home/', views.home, name='blog-home'),
     path('recent/', views.recent, name='blog-recent'),
-    path('help/', views.help, name='blog-help'),
+    path('help/', views.helpme, name='blog-help'),
     path('profile/', views.profile, name='blog-profile'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import authenticate
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
@@ -16,7 +15,7 @@ def recent(request):
     return render(request, 'blog/recent.html')
 
 
-def help(request):
+def helpme(request):
     return render(request, 'blog/help.html')
 
 
@@ -24,7 +23,7 @@ def profile(request):
     return render(request, 'blog/profile.html')
 
 
-def login(request):
+def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -40,5 +39,5 @@ def login(request):
             else:
                 return HttpResponse('Invalid login')
     else:
-        form = LoginForm
+        form = LoginForm()
     return render(request, 'blog/login.html', {'form': form})
