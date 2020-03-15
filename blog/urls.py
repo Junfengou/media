4f38f2fab6fd2660e18fb1d01fdae266b696d803
Junfeng ou <junfengou@gmail.com>
2020-03-15 16:39:52 -0500

    added email function

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,21 @@ urlpatterns = [
     path('recent/', views.recent, name='blog-recent'),
     path('help/', views.helpme, name='blog-help'),
     path('profile/', views.profile, name='blog-profile'),
+
+    # Path below are for reset passwords
+    path('resetpassword/',
+         auth_views.PasswordResetView.as_view(template_name='blog/resetpassword.html'),
+         name='reset_password'),
+
+    path('resetpasswordsent/',
+         auth_views.PasswordResetDoneView.as_view(template_name='blog/resetpasswordsent.html'),
+         name='password_reset_done'),
+
+    path('resetpassword/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name='blog/resetpasswordform.html'),
+         name='password_reset_confirm'),
+
+    path('resetpasswordcomplete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='blog/resetpasswordcomplete.html'),
+         name='password_reset_complete'),
 ]
